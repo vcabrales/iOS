d9cfbd271e06a49c2dabd6a20a560d63222d4e3a
Miguel Beltran <user@users-computer.local>
2016-09-10 18:04:00 -0500

    Delete button

--- First Class/First Class/Custom/customTVC.swift
+++ First Class/First Class/Custom/customTVC.swift
@@ -15,6 +15,27 @@ class customTVC: UITableViewCell {
     @IBOutlet weak var myTitle: UILabel!
     @IBOutlet weak var mySubtitle: UILabel!
     
+    @IBAction func Delete(sender: AnyObject) {
+        let File = "/" + myTitle.text! + ".strings"
+        var path = "/Users/user/Desktop/Swift/iOS/iOS/First Class/First Class"
+        path.appendContentsOf(File)
+        
+        
+        //if let filepath = NSBundle.mainBundle().pathForResource(File, ofType: "strings") {
+            //print(filepath)
+            do {
+                let fileManager = NSFileManager.defaultManager()
+                try fileManager.removeItemAtPath(path)
+            }
+                catch let error as NSError {
+                print("Ooops! Something went wrong: \(error)")
+            }
+        //} else {
+        //    print("\(File) not found!")
+        //}
+        
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
